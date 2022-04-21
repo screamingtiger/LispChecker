eed26210fd9b60b18168a3ed5c941e9df0f2d672
Joey Thompson <nwjt22@gmail.com>
2022-04-21 10:00:59 -0500

    add test

--- app/src/test/groovy/Lisp/LispCheckerSpec.groovy
+++ app/src/test/groovy/Lisp/LispCheckerSpec.groovy
@@ -10,18 +10,21 @@ class LispCheckerSpec extends Specification {
         String inputNotOK = "))((";
         String inputNotOK2 = "(()))(";
         String inputNull = null;
+        String inputEmpty = "";
 
         when:
         boolean  inputOkResult = LispChecker.validate(inputOk);
         boolean  inputNotOKResult = LispChecker.validate(inputNotOK);
         boolean  inputNotOK2Result = LispChecker.validate(inputNotOK2);
         boolean  inputNullResult = LispChecker.validate(inputNull);
+        boolean  inputEmptyResult = LispChecker.validate(inputEmpty);
 
         then:
         inputOkResult;
         !inputNotOKResult;
         !inputNotOK2Result;
         inputNullResult;
+        inputEmptyResult;
 
 
     }
